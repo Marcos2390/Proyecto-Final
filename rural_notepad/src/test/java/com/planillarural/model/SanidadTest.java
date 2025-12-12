fa62c56fa0fb761ce7aef852030ad3dcc4171dda
Marcos Silva <marcosdamian19902015@gmail.com>
2025-12-12 17:40:40 -0300

    Primer commit desde cero con cambios actualizados

--- rural_notepad/src/test/java/com/planillarural/model/SanidadTest.java
+++ rural_notepad/src/test/java/com/planillarural/model/SanidadTest.java
@@ -2,17 +2,29 @@ package com.planillarural.model;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.planillarural.service.RegistroService;
 
 class SanidadTest {
 
+    private RegistroService service;
+
+    @BeforeEach
+    void setup() {
+        service = new RegistroService();
+    }
+
     @Test
-    void sanidadStoresDataCorrectly() { // prueba del constructor de Sanidad
-        Sanidad s = new Sanidad(10, "Vacuna Aftosa", "01/05/2025");
+    void registrarSanidad_creaCorrectamente() {
+        Sanidad s = new Sanidad(10, "Aftosa", "01/05/2025");
+
+        service.registrarSanidad(s);
 
         assertAll(
-                () -> assertEquals(10, s.getIdAnimal()), // verifica idAnimal
-                () -> assertEquals("Vacuna Aftosa", s.getVacuna()), // verifica vacuna
+                () -> assertEquals(1, service.listarSanidad().size()), // verifica tamaño de la lista
+                () -> assertEquals("Aftosa", service.listarSanidad().get(0).getVacuna()), // verifica vacuna
                 () -> assertEquals("01/05/2025", s.getFecha())); // verifica fecha
     }
 }
